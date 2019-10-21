771990746ac8c48581b544c2cc3b132f8f4bd8b8
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-10-22 00:42:38 +0700

    Using fanex data to connect database

--- ChessResult/src/ChessResult.Site/Global.asax.cs
+++ ChessResult/src/ChessResult.Site/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Fanex.Data;
 
 namespace ChessResult.Site
 {
@@ -16,6 +17,12 @@ namespace ChessResult.Site
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            DbSettingProviderManager
+                .StartNewSession()
+                .UseDefaultConnectionStringProvider()
+                .UseDefaultDbSettingProvider(resourcePath: "~/App_Data/SPs")
+                .Run();
         }
     }
 }
